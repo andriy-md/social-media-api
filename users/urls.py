1a14b0532d8e78b5f9db9c82c370c76601c55043
Andriy <an_mydel@gmail.com>
2023-09-10 11:02:43 +0300

    Add User create endpoint

--- users/urls.py
+++ users/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
+from users.views import CreateUserView
+
 urlpatterns = [
     path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    path("register/", CreateUserView.as_view(), name="create")
 ]
 
 app_name = "user"
