915d6cebe9b2bcfa8d6e31306c115ba4a1a859c6
Andriy <an_mydel@gmail.com>
2023-09-12 20:38:05 +0300

    Create Hashtag and Post models and register them in admit.py

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
-# Create your models here.
+from profiles.models import Profile
+
+
+class Hashtag(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Post(models.Model):
+    text = models.TextField()
+    author = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    hashtags = models.ManyToManyField(Hashtag, related_name="posts")
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    @property
+    def text_preview(self):
+        if len(self.text) < 32:
+            return self.text
+        return self.text[:31]
+
+    def __str__(self):
+        return f'"{self.text_preview}" by {self.author}'
