98d4d9243da85b78f679219f53273d30c8cb952e
Andriy <an_mydel@gmail.com>
2023-09-14 22:14:16 +0300

    Add filtering Posts by author and hashtag

--- posts/views.py
+++ posts/views.py
@@ -1,6 +1,4 @@
-from django.shortcuts import get_object_or_404
-from rest_framework import status
-from rest_framework.response import Response
+from django.db.models import Q
 from rest_framework.viewsets import ModelViewSet
 
 from posts.models import Post
@@ -17,6 +15,26 @@ class PostViewSet(ModelViewSet):
         if self.action == "list":
             return PostListSerializer
         return PostCreateSerializer
+
+    def get_queryset(self):
+        queryset = Post.objects.prefetch_related(
+            "hashtags").select_related("author").all()
+
+        if self.request.query_params.get("author"):
+            query = Q()
+            searched_profiles = self.request.query_params["author"]
+            searched_profiles = searched_profiles.split(",")
+            for profile in searched_profiles:
+                query |= Q(author__user__email__icontains=profile)
+            queryset = queryset.filter(query)
+
+        if self.request.query_params.get("hashtag"):
+            searched_tags = self.request.query_params.get("hashtag")
+            searched_tags = searched_tags.split(",")
+            for tag in searched_tags:
+                queryset = queryset.filter(hashtags__name=tag)
+
+        return queryset
 
     def perform_create(self, serializer):
         profile = Profile.objects.get(user=self.request.user)
